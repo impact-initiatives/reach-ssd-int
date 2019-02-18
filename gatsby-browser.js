25c1fc74e721e77b882b46bc1ad507986e8a8ffa
Max Malynowsky <maxmalynowsky@gmail.com>
2019-02-18 23:00:42 +0300

    auto-refresh on update

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,21 +1,12 @@
-import { notification } from 'antd';
+import { message } from 'antd';
 
 import wrapWithProvider from './src/utils/wrap-with-provider';
 export const wrapRootElement = wrapWithProvider;
 
 export const onServiceWorkerUpdateFound = () => {
-  notification.open({
-    message: 'Update Downloading',
-    description: 'A new version of this site is downloading.',
-    duration: 0,
-  });
+  message.loading('Update downloading...', 0);
 };
 
 export const onServiceWorkerUpdateReady = () => {
-  notification.open({
-    message: 'Update Downloaded',
-    description:
-      'A new version of this site is available. Please refresh to get the latest content.',
-    duration: 0,
-  });
+  window.location.reload(true);
 };
