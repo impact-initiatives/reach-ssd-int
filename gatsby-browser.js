e46c315a4777b7fa92489e347b5d5c578e4874bb
Max Malynowsky <maxmalynowsky@gmail.com>
2019-02-18 22:27:48 +0300

    add service worker notigication to root

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,2 +1,13 @@
+import { notification } from 'antd';
+
 import wrapWithProvider from './src/utils/wrap-with-provider';
 export const wrapRootElement = wrapWithProvider;
+
+export const onServiceWorkerUpdateReady = () => {
+  notification.open({
+    message: 'Update Downloaded',
+    description:
+      'A new version of this site is available. Please refresh to get the latest content',
+    duration: 0,
+  });
+};
