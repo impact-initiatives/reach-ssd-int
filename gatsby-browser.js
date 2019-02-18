66551543450023ad097ead1d1dd561db1f2f91a0
Max Malynowsky <maxmalynowsky@gmail.com>
2019-02-18 22:33:51 +0300

    commit update notification

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,6 +3,15 @@ import { notification } from 'antd';
 import wrapWithProvider from './src/utils/wrap-with-provider';
 export const wrapRootElement = wrapWithProvider;
 
+export const onServiceWorkerUpdateFound = () => {
+  notification.open({
+    message: 'Update Downloading',
+    description:
+      'A new version of this site is downloading.',
+    duration: 0,
+  });
+};
+
 export const onServiceWorkerUpdateReady = () => {
   notification.open({
     message: 'Update Downloaded',
